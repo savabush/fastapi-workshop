fd7c686bf40225b21e299a0f0733b08b7e1ebdd2
savabush <vatka1337@gmail.com>
2022-08-07 17:13:41 +0700

    MOD | Added model authentication and base api for auth

---
+++ api/auth.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from models.auth import UserCreate, Token
+
+router = APIRouter(prefix='/auth')
+
+
+@router.post('/sign-up', response_model=Token)
+def sign_up(user_data: UserCreate):
+    pass
+
+
+@router.post('/sign-in', response_model=Token)
+def sign_in(form_data: OAuth2PasswordRequestForm = Depends()):
+    pass
